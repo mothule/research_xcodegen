978f400222976eb32cd7afa70ee50a7cd0701231
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-07-28 17:22:52 +0900

    Add iOSMinimumProjects

---
+++ ios_minimum_proj/iOSMinimumProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hoge
+//
+//  Created by motoki kawakami on 2020/07/28.
+//  Copyright © 2020 mothule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
