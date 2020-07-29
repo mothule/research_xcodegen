11c81d3b63e33b0654f5e99fd0972161ddb6cf5f
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-07-29 19:09:26 +0900

    Prepare EmbeddedFramework proj

---
+++ embedded_framework/EmbeddedFramework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmbededdFramework
+//
+//  Created by motoki kawakami on 2020/07/29.
+//  Copyright © 2020 mothule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
